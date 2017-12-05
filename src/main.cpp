4c24cc4e10d72e38a1111e3316307ec752464afa
Euxiniar <charles.ago37@gmail.com>
2017-12-05 13:11:37 +0100

    device initialized

--- src/main.cpp
+++ src/main.cpp
@@ -21,7 +21,7 @@ int main()
 	app_info.engineVersion = 1;
 	app_info.apiVersion = VK_API_VERSION_1_0;
 
-	// initialize the VKInstanceCreateInfo structure
+	// initialize the VkInstanceCreateInfo structure
 	VkInstanceCreateInfo inst_info = {};
 	inst_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
 	inst_info.pNext = NULL;
@@ -35,6 +35,7 @@ int main()
 	VkInstance inst;
 	VkResult res;
 
+	// create Vkinstance
 	res = vkCreateInstance(&inst_info, NULL, &inst);
 	if (res == VK_ERROR_INCOMPATIBLE_DRIVER) {
 		std::cout << "cannot find a compatible Vulkan ICD\n";
@@ -45,6 +46,7 @@ int main()
 		exit(-1);
 	}
 
+	// enumerate devices
 	uint32_t gpu_count = 1;
 	res = vkEnumeratePhysicalDevices(inst, &gpu_count, NULL);
 	assert(gpu_count);
@@ -54,6 +56,55 @@ int main()
 	res = vkEnumeratePhysicalDevices(inst, &gpu_count, gpus.data());
 	assert(!res && gpu_count >= 1);
 
+	// initialize the VkDeviceQueueCreateInfo structure
+	uint32_t queue_family_count = 1;
+	VkDeviceQueueCreateInfo queue_info = {};
+
+	vkGetPhysicalDeviceQueueFamilyProperties(gpus[0], &queue_family_count, NULL);
+	assert(queue_family_count >= 1);
+
+	std::vector<VkQueueFamilyProperties> queue_props;
+	queue_props.resize(queue_family_count);
+	vkGetPhysicalDeviceQueueFamilyProperties(gpus[0], &queue_family_count, queue_props.data());
+	assert(queue_family_count >= 1);
+
+	bool found = false;
+	for (unsigned int i = 0; i < queue_family_count; i++)
+	{
+		if (queue_props[i].queueFlags & VK_QUEUE_GRAPHICS_BIT)
+		{
+			queue_info.queueFamilyIndex = i;
+			found = true;
+			break;
+		}
+	}
+	assert(found);
+	assert(queue_family_count >= 1);
+
+	float queue_priorities[1] = { 0.0 };
+	queue_info.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+	queue_info.pNext = NULL;
+	queue_info.queueCount = 1;
+	queue_info.pQueuePriorities = queue_priorities;
+
+
+	//initialize VkDeviceCreateInfo structure
+	VkDeviceCreateInfo device_info = {};
+	device_info.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+	device_info.pNext = NULL;
+	device_info.queueCreateInfoCount = 1;
+	device_info.pQueueCreateInfos = &queue_info;
+	device_info.enabledExtensionCount = 0;
+	device_info.ppEnabledExtensionNames = NULL;
+	device_info.enabledLayerCount = 0;
+	device_info.ppEnabledLayerNames = NULL;
+	device_info.pEnabledFeatures = NULL;
+
+	VkDevice device;
+	res = vkCreateDevice(gpus[0], &device_info, NULL, &device);
+	assert(res == VK_SUCCESS);
+
+	vkDestroyDevice(device, NULL);
 	vkDestroyInstance(inst, NULL);
 
 	_getch();
