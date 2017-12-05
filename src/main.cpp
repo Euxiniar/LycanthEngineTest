93056e7f452ad2bfa466089fadacbff9a6f09ca7
Euxiniar <charles.ago37@gmail.com>
2017-12-05 10:23:51 +0100

    instance initialization

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,49 @@
+#include <vulkan\vulkan.h>
+#include <iostream>
+#include <conio.h>
+
+#define APP_NAME "LycanthEngine"
+#define ENGINE_NAME "LycanthEngine";
 
 int main()
 {
+	std::cout << "Hello World !" << std::endl;
+
+	// initialize the VkApplicationInfo structure
+	VkApplicationInfo app_info = {};
+	app_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+	app_info.pNext = NULL;
+	app_info.pApplicationName = APP_NAME;
+	app_info.applicationVersion = 1;
+	app_info.pEngineName = ENGINE_NAME;
+	app_info.engineVersion = 1;
+	app_info.apiVersion = VK_API_VERSION_1_0;
+
+	// initialize the VKInstanceCreateInfo structure
+	VkInstanceCreateInfo inst_info = {};
+	inst_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+	inst_info.pNext = NULL;
+	inst_info.flags = 0;
+	inst_info.pApplicationInfo = &app_info;
+	inst_info.enabledExtensionCount = 0;
+	inst_info.ppEnabledExtensionNames = NULL;
+	inst_info.enabledLayerCount = 0;
+	inst_info.ppEnabledLayerNames = NULL;
+
+	VkInstance inst;
+	VkResult res;
+
+	res = vkCreateInstance(&inst_info, NULL, &inst);
+	if (res == VK_ERROR_INCOMPATIBLE_DRIVER) {
+		std::cout << "cannot find a compatible Vulkan ICD\n";
+		exit(-1);
+	}
+	else if (res) {
+		std::cout << "unknown error\n";
+		exit(-1);
+	}
+
+	vkDestroyInstance(inst, NULL);
+	_getch();
 	return 0;
 }
